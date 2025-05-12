b896d94b3083ef660565814151ec6c8602fc49c4
Kacper <kgrzesik17@gmail.com>
2025-05-12 23:21:06 +0200

    querying and associating related models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,8 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['review', 'rating'];
+
     public function book() {
         // return Book parent
         return $this->belongsTo(Book::class);
