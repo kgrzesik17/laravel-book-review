2d08f0511400feadc8c19f2cfda10b0c4c79be00
Kacper <kgrzesik17@gmail.com>
2025-05-12 23:02:08 +0200

    added factories and seeders for book and review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    use HasFactory;
+
     public function book() {
         // return Book parent
         return $this->belongsTo(Book::class);
