5826c300a72811a2893aacd5ddf1dbf82519978a
Kacper <kgrzesik17@gmail.com>
2025-05-28 20:22:35 +0200

    displaying the book list

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,70 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8">
-  <title>Book Reviews</title>
-  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
+@section('content')
+    <h1 class="mb-10 text-2xl">Books</h1>
 
-  {{-- blade-formatter-disable --}}
-  <style type="text/tailwindcss">
-    .btn {
-      @apply bg-white rounded-md px-4 py-2 text-center font-medium text-slate-500 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 h-10;
-    }
+    <form action="">
 
-    .input {
-      @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none rounded-md border-slate-300;
-    }
+    </form>
 
-    .filter-container {
-      @apply mb-4 flex space-x-2 rounded-md bg-slate-100 p-2;
-    }
-
-    .filter-item {
-      @apply flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium text-slate-500;
-    }
-
-    .filter-item-active {
-      @apply bg-white shadow-sm text-slate-800 flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium;
-    }
-
-    .book-item {
-      @apply text-sm rounded-md bg-white p-4 leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
-    }
-
-    .book-title {
-      @apply text-lg font-semibold text-slate-800 hover:text-slate-600;
-    }
-
-    .book-author {
-      @apply block text-slate-600;
-    }
-
-    .book-rating {
-      @apply text-sm font-medium text-slate-700;
-    }
-
-    .book-review-count {
-      @apply text-xs text-slate-500;
-    }
-
-    .empty-book-item {
-      @apply text-sm rounded-md bg-white py-10 px-4 text-center leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
-    }
-
-    .empty-text {
-      @apply font-medium text-slate-500;
-    }
-
-    .reset-link {
-      @apply text-slate-500 underline;
-    }
-  </style>
-  {{-- blade-formatter-enable --}}
-</head>
-
-<body class="container mx-auto mt-10 mb-10 max-w-3xl">
-  @yield('content')
-</body>
-
-</html>
+    <ul>
+        @forelse($books as $book)
+            <li class="mb-4">
+            <div class="book-item">
+                <div
+                class="flex flex-wrap items-center justify-between">
+                <div class="w-full flex-grow sm:w-auto">
+                    <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+                    <span class="book-author">{{ $book->author }}</span>
+                </div>
+                <div>
+                    <div class="book-rating">
+                    {{ number_format($book->reviews_avg_rating, 1) }}
+                    </div>
+                    <div class="book-review-count">
+                    out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
+                    </div>
+                </div>
+                </div>
+            </div>
+            </li>
+        @empty
+            <li class="mb-4">
+            <div class="empty-book-item">
+                <p class="empty-text">No books found</p>
+                <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
+            </div>
+            </li>
+        @endforelse
+    </ul>
+@endsection
